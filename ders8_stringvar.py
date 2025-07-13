afc31df5d902b755fc09ec1e0746ba5c34b1ae57
Yigit581 <yigit.yakar58@gmail.com>
2025-07-13 15:43:15 +0300

    intvar added

---
+++ ders8_stringvar.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import random
+import string
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("StringVar, IntVar Projesi")
+root.geometry("300x400")
+
+sayi = tk.IntVar()
+sayi.set(1)
+
+root.mainloop()
