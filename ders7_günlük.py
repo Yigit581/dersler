e699e634284f6780047907448f27976ce7e3346d
Yigit581 <yigit.yakar58@gmail.com>
2025-07-20 15:54:57 +0300

    günlük eklendi

--- ders7_günlük.py
+++ ders7_günlük.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
+from tkinter import colorchooser
+from PIL import Image,ImageTk
+
+
+def renk_sec():
+    secilen_renk = colorchooser.askcolor(title= "Renk Seçiniz")
+    if secilen_renk [1]:
+        metin_kutusu.config(bg= secilen_renk[1])
+
+def metin_renk_sec():
+    secilen_renk = colorchooser.askcolor(title= "Renk Seçiniz")
+    if secilen_renk [1]:
+        metin_kutusu.config(fg= secilen_renk[1])
 
 def kaydet():
     metin = metin_kutusu.get("1.0", "end-1c")
@@ -19,9 +32,9 @@ def kaydet():
 
 root = tk.Tk()
 root.title("Günlük projesi")
-root.geometry("400x300")
+root.geometry("400x400")
 
-label = tk.Label(root, text= "Bu gün ne hissediyorsun")
+label = tk.Label(root, text= "Bu gün ne hissediyorsun", )
 label.pack(pady=10)
 
 metin_kutusu = tk.Text(root, height=10, width=40)
@@ -29,5 +42,19 @@ metin_kutusu.pack(pady=10)
 
 button = tk.Button(root, text= "Kaydet", command= kaydet)
 button.pack(pady=10)
+
+image = Image.open(f"assets\color-pallete.png")
+image = image.resize((30,30))
+image = ImageTk.PhotoImage(image)
+
+renk_button = tk.Button(root, text= "Renk Seç", command= renk_sec, image=image)
+renk_button.pack(pady=10, padx=10, side="left")
+
+text_image = Image.open(r"assets\font.png")
+text_image = text_image.resize((30,30))
+text_image = ImageTk.PhotoImage(text_image)
+
+metin_renk_button = tk.Button(root, command= metin_renk_sec, image=text_image)
+metin_renk_button.pack(pady=10, padx=10, side="left")
 
 root.mainloop()
